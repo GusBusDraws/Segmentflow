e863767e165971731145410b591039ac75e1e232
Scott Runnels <srunnels@srconsult.com>
2022-08-02 13:30:18 -0600

    Moving to ASCII-based comparison for STD files

--- testing/cases/case_01/runCase.py
+++ testing/cases/case_01/runCase.py
@@ -8,6 +8,28 @@ import glob
 from pathlib import Path
 import subprocess
 
+def stripFirstAndLastLines(filename):
+
+    # Read file into list, L, except not the first and last lines
+    
+    L = []
+    
+    f = open(Path(filename),'r')
+    for line in f:
+        L.append(line.replace('\n',''))
+    f.close()
+
+    # Read file into list, L
+    
+    f = open(filename,'w')
+    for i in range(0,len(L)):
+        if i > 0 and i < len(L)-1:
+            print(L[i],file=f)
+    f.close()
+
+
+    
+
 if __name__ == '__main__':
     
     os.environ["PATH"] += os.pathsep + './'
@@ -25,5 +47,16 @@ if __name__ == '__main__':
     p = subprocess.run(['python3' , Path('../../../segment.py'),('-fsegment.yml') ],stdout = tty,stderr=tty_err)
     tty.close()
     tty_err.close()
+        
+    # Convert binary stl to ascii
+    
+    p = subprocess.run(['stl2ascii','segmented_63.stl','segmented_63.txt'])
+    p = subprocess.run(['stl2ascii','segmented_02.stl','segmented_02.txt'])
+        
+        
+    # Strip off the first line, which has a time stamp
+
+    stripFirstAndLastLines('segmented_02.txt')
+    stripFirstAndLastLines('segmented_63.txt')
         
         
