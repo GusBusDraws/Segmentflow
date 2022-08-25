de9bd03e39112401f93a65453089288ffeb2668a
cgusb <cgusbecker@gmail.com>
2022-08-25 14:30:36 -0400

    Fix bug in 'load_inputs()': convert image directory to Path object for Path operations

--- segment.py
+++ segment.py
@@ -263,8 +263,9 @@ def load_images(
         (depending on return_3d_array), or if also_return_names is True, 
         list containing names of images from filenames is also returned.
     """
+    img_dir = Path(img_dir)
     img_path_list = [
-        path for path in Path(img_dir).glob(f'*{file_suffix}')
+        path for path in img_dir.glob(f'*{file_suffix}')
     ]
     img_path_list.sort()
     if slice_crop is None:
