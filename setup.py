c4e98fd87a46c283a811f004f1836ada711f3b8d
cgusb <cgusbecker@gmail.com>
2023-06-29 16:07:04 -0400

    Increment version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="segmentflow",
-    version="0.0.2",
+    version="0.0.3",
     install_requires=[
         'imageio >= 2.21.0',
         'matplotlib',
