81c5ab530a672855a6eb73998ec05b958d245d31
cgusb <cgusbecker@gmail.com>
2023-08-07 17:36:38 -0400

    Update single grain workflow to work with input file

--- segmentflow/workflows/F50_single_grain_segment.py
+++ segmentflow/workflows/F50_single_grain_segment.py
@@ -17,82 +17,43 @@ WORKFLOW_DESCRIPTION = (
 )
 
 CATEGORIZED_INPUT_SHORTHANDS = {
-    'Files' : {
-        'in_dir_path'  : 'Input dir path',
-        'file_suffix'  : 'File suffix',
-        'slice_crop'   : 'Slice crop',
-        'row_crop'     : 'Row crop',
-        'col_crop'     : 'Column crop',
-        'spatial_res'  : 'Pixel size',
-        'out_dir_path' : 'Path to save output dir',
-        'out_prefix'   : 'Output prefix',
-        'overwrite'    : 'Overwrite files'
+    'A. Input' : {
+        'in_dir_path'  : '01. Input dir path',
+        'file_suffix'  : '02. File suffix',
+        'slice_crop'   : '03. Slice crop',
+        'row_crop'     : '04. Row crop',
+        'col_crop'     : '05. Column crop',
+        'spatial_res'  : '06. Pixel size',
     },
-    'View' : {
-        'view_slices'   : 'Slices to view',
-        'view_raw'      : 'View raw images',
-        'view_pre'      : 'View preprocessed images',
-        'view_semantic' : 'View semantic images',
-        'view_labeled'  : 'View labeled images',
+    'B. Processing' : {
+        'rm_min_size'   : '01. Minimum noise volume to remove',
+        'ero_dil_iters' : '02. Number of erosion-dilation iterations',
     },
-    'Preprocess' : {
-        'pre_seg_med_filter' : 'Apply median filter',
-        'rescale_range'      : 'Rescale intensity range',
-    },
-    'Segmentation' : {
-        'thresh_nbins'      : 'Histogram bins for calculating thresholds',
-        'view_thresh_hist'  : 'View histogram with threshold values',
-        'thresh_hist_ylims' : 'Upper and lower y-limits of histogram',
-        'perform_seg'       : 'Perform instance segmentation',
-        'min_peak_dist'     : 'Min peak distance',
-        'exclude_borders'   : 'Exclude border particles',
-        'save_voxels'       : 'Save labeled voxels'
-    },
-    'STL' : {
-        'create_stls'          : 'Create STL files',
-        'suppress_save_msg'    : 'Suppress save message for each STL file',
-        'n_erosions'           : 'Number of pre-surface meshing erosions',
-        'post_seg_med_filter'  : 'Smooth voxels with median filtering',
-        'voxel_step_size'      : 'Marching cubes voxel step size',
-        'mesh_smooth_n_iters'  : 'Number of smoothing iterations',
-        'mesh_simplify_n_tris' : 'Target number of triangles/faces',
-        'mesh_simplify_factor' : 'Simplification factor per iteration',
+    'C. Output' : {
+        'overwrite'    : '01. Overwrite files',
+        'out_prefix'   : '02. Output prefix',
+        'nslices'      : '03. Number of slices in checkpoint plots',
+        'out_dir_path' : '04. Path to save output dir',
+        'save_stl'     : '05. Save STL file',
+        'save_voxels'  : '06. Save voxel TIF stack'
     },
 }
 
 DEFAULT_VALUES = {
-    'in_dir_path'          : 'REQUIRED',
-    'file_suffix'          : 'tiff',
-    'slice_crop'           : None,
-    'row_crop'             : None,
-    'col_crop'             : None,
-    'out_dir_path'         : 'REQUIRED',
-    'out_prefix'           : '',
-    'overwrite'            : False,
-    'view_slices'          : True,
-    'view_raw'             : True,
-    'view_pre'             : True,
-    'view_semantic'        : True,
-    'view_labeled'         : True,
-    'pre_seg_med_filter'   : False,
-    'rescale_range'        : None,
-    'thresh_nbins'         : 256,
-    'view_thresh_hist'     : True,
-    'thresh_hist_ylims'    : [0, 2e7],
-    'perform_seg'          : True,
-    'min_peak_dist'        : 6,
-    'exclude_borders'      : True,
-    'save_voxels'          : True,
-    'create_stls'          : True,
-    'suppress_save_msg'    : True,
-    'n_erosions'           : 0,
-    'post_seg_med_filter'  : False,
-    'spatial_res'          : 1,
-    'voxel_step_size'      : 1,
-    'mesh_smooth_n_iters'  : None,
-    'mesh_simplify_n_tris' : None,
-    'mesh_simplify_factor' : None,
-    'seg_fig_show'         : False,
+    'in_dir_path'   : 'REQUIRED',
+    'file_suffix'   : '.tif',
+    'slice_crop'    : None,
+    'row_crop'      : None,
+    'col_crop'      : None,
+    'spatial_res'   : 1,
+    'rm_min_size'   : 500,
+    'ero_dil_iters' : 8,
+    'out_dir_path'  : 'REQUIRED',
+    'out_prefix'    : '',
+    'overwrite'     : True,
+    'nslices'       : 5,
+    'save_stl'      : True,
+    'save_voxels'   : True,
 }
 
 #~~~~~~~~~~#
@@ -102,24 +63,15 @@ def workflow(argv):
     #-----------------------------------------------------#
     # Get command-line arguments and read YAML input file #
     #-----------------------------------------------------#
-    # ui = segment.process_args(
-    #     argv, WORKFLOW_NAME, WORKFLOW_DESCRIPTION, CATEGORIZED_INPUT_SHORTHANDS,
-    #     DEFAULT_VALUES
-    # )
-    ui = {}
-    ui['in_dir_path'] = r'C:\Users\gusb\Research\mhe-analysis\data\F50_1_Scan_1'
-    ui['file_suffix'] = '.tif'
-    ui['slice_crop'] = [340, 600]
-    ui['row_crop'] = [600, 1250]
-    ui['col_crop'] = [600, 1250]
-    ui['out_dir_path'] = r'C:\Users\gusb\Research\mhe-analysis\results\F50_1_Scan_1'
-    ui['out_prefix'] = 'F50_1_Scan_1'
-    ui['overwrite'] = True
-    ui['spatial_res'] = 0.00109
-    ui['nslices'] = 4
-    ui['rm_min_size'] = 500
-    ui['ero_dil_iters'] = 8
+    ui = segment.process_args(
+        argv, WORKFLOW_NAME, WORKFLOW_DESCRIPTION, CATEGORIZED_INPUT_SHORTHANDS,
+        DEFAULT_VALUES
+    )
 
+    n_fig_digits = 2
+    fig_n = 0
+    if not Path(ui['out_dir_path']).exists():
+        Path(ui['out_dir_path']).mkdir(parents=True)
     #-------------#
     # Load images #
     #-------------#
@@ -136,9 +88,11 @@ def workflow(argv):
         imgs,
         nslices=ui['nslices'],
         fig_w=7.5,
-        dpi=100
+        dpi=300
     )
-    plt.show()
+    plt.savefig(
+        Path(ui['out_dir_path'])
+        / f'{str(fig_n).zfill(n_fig_digits)}-raw-imgs.png')
 
     #-------------------#
     # Preprocess images #
@@ -149,13 +103,6 @@ def workflow(argv):
         imgs, median_filter=True,
         rescale_intensity_range=None
     )
-    # Plot histogram
-    hist, bin_edges = np.histogram(imgs_med)
-    fig, ax = plt.subplots(dpi=150)
-    ax.plot(bin_edges[1:], hist, lw=1)
-    for val in thresh_vals:
-        ax.axvline(val, c='red', zorder=0)
-    plt.show()
 
     #-----------------------#
     # Semantic segmentation #
@@ -163,27 +110,44 @@ def workflow(argv):
     imgs_binarized, thresh_vals = segment.binarize_multiotsu(
         imgs_med, n_otsu_classes=2
     )
+    # Plot histogram
+    hist, bin_edges = np.histogram(imgs_med)
+    fig, ax = plt.subplots(dpi=300)
+    ax.plot(bin_edges[1:], hist, lw=1)
+    for val in thresh_vals:
+        ax.axvline(val, c='red', zorder=0)
+    fig_n += 1
+    plt.savefig(
+        Path(ui['out_dir_path'])
+        / f'{str(fig_n).zfill(n_fig_digits)}-hist.png')
     # zyx
     fig, axes = view.plot_slices(
         imgs_binarized,
         nslices=ui['nslices'],
         fig_w=7.5,
-        dpi=100
+        dpi=300
     )
+    fig_n += 1
+    plt.savefig(
+        Path(ui['out_dir_path'])
+        / f'{str(fig_n).zfill(n_fig_digits)}-bw-yx.png')
     # Plot rotated crop - yxz
     imgs_binarized_xz = np.rot90(imgs_binarized, axes=(2, 0))
-    print(f'{imgs_binarized_xz.shape=}')
     fig, axes = view.plot_slices(
         imgs_binarized_xz,
         nslices=ui['nslices'],
         fig_w=7.5,
-        dpi=100
+        dpi=300
     )
-    plt.show()
+    fig_n += 1
+    plt.savefig(
+        Path(ui['out_dir_path'])
+        / f'{str(fig_n).zfill(n_fig_digits)}-bw-xz.png')
 
     #-------------------------------------------#
     # Remove small particles outside sand grain #
     #-------------------------------------------#
+    print(f"Clearing noise smaller than {ui['rm_min_size']} voxels...")
     imgs_cleaned = np.zeros_like(imgs_binarized)
     for n in range(imgs_binarized.shape[0]):
         imgs_cleaned[n, ...] = morphology.remove_small_objects(
@@ -193,30 +157,24 @@ def workflow(argv):
     imgs_filled = np.zeros_like(imgs_cleaned)
     for n in range(imgs_cleaned.shape[0]):
         imgs_filled[n, ...] = ndi.binary_fill_holes(imgs_cleaned[n, ...])
-    # Plot images with noise cleaned
-    fig, axes = view.plot_slices(
-        imgs_cleaned,
-        nslices=ui['nslices'],
-        imgs_per_row=4,
-        fig_w=7.5,
-        dpi=100
-    )
-    plt.show()
     # Plot filled particle
     fig, axes = view.plot_slices(
         imgs_filled,
         nslices=ui['nslices'],
-        imgs_per_row=4,
         fig_w=7.5,
-        dpi=100
+        dpi=300
     )
-    plt.show()
+    fig_n += 1
+    plt.savefig(
+        Path(ui['out_dir_path'])
+        / f'{str(fig_n).zfill(n_fig_digits)}-filled.png')
 
     #---------------------------------#
     # Remove reconstruction artifacts #
     #---------------------------------#
     # Ring-like artifacts extruding from edge of sand grain:
     # Attempt to remove by eroding and then "re-roding" (dilating) region
+    print('Removing reconstruction artifacts...')
     imgs_eroded = ndi.binary_erosion(
         imgs_filled,
         iterations=ui['ero_dil_iters']
@@ -245,20 +203,25 @@ def workflow(argv):
     fig, axes = view.plot_slices(
         imgs_eroded,
         nslices=ui['nslices'],
-        imgs_per_row=4,
         fig_w=7.5,
-        dpi=100
+        dpi=300
     )
+    fig_n += 1
+    plt.savefig(
+        Path(ui['out_dir_path'])
+        / f'{str(fig_n).zfill(n_fig_digits)}-eroded-yx.png')
     # yxz
-    imgs_eroded_xz = np.rot90(imgs_eroded, axes=(2, 0)),
+    imgs_eroded_xz = np.rot90(imgs_eroded, axes=(2, 0))
     fig, axes = view.plot_slices(
         imgs_eroded_xz,
         nslices=ui['nslices'],
-        imgs_per_row=4,
         fig_w=7.5,
-        dpi=100
+        dpi=300
     )
-    plt.show()
+    fig_n += 1
+    plt.savefig(
+        Path(ui['out_dir_path'])
+        / f'{str(fig_n).zfill(n_fig_digits)}-eroded-xz.png')
 
     #--------------#
     # Save outputs #
@@ -269,14 +232,16 @@ def workflow(argv):
             imgs_filled_labeled,
             ui['out_dir_path'],
             ui['out_prefix'],
-            make_new_save_dir=False,
+            make_new_save_dir=True,
             spatial_res=ui['spatial_res'],
             stl_overwrite=ui['overwrite']
         )
     # save images
     if ui['save_voxels']:
+        imgs_labeled_8bit = imgs_filled_labeled.astype(np.ubyte)
+        imgs_labeled_8bit[imgs_labeled_8bit == 1] = 255
         segment.save_images(
-            imgs_filled_labeled,
+            imgs_labeled_8bit,
             Path(ui['out_dir_path']) / f"{ui['out_prefix']}_labeled_voxels"
         )
 
