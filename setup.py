66ec6f19db0446fbe2083e9f30616742b7e151e7
cgusb <cgusbecker@gmail.com>
2023-11-06 15:56:44 -0500

    Remove pkg_resources imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="segmentflow",
-    version="0.0.3",
+    version="0.0.4",
     install_requires=[
         'imageio >= 2.21.0',
         'matplotlib',
